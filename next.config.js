7d09cd9ab6d39aec36952e3d93a5d816cc36a082
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-01-09 19:54:32 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-module.exports = nextConfig
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+});
+
+module.exports = nextConfig;
