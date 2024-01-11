6fe6b9c1804ca92864c16fdd6678deb64be40fcf
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-01-11 18:05:20 -0300

    wip

---
+++ src/components/SocialCard.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export default function SocialCard() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        <CarouselItem>
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex aspect-square items-center justify-center p-6">
+                <span className="text-4xl font-semibold">teste</span>
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
